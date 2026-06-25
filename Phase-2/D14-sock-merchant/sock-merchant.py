ac11569c682b5e210bb1695212154b064c9087f8
Nezira <neziraworku198@gmail.com>
2026-06-25 16:12:46 -0700

    added day 14 challenge solution

---
+++ Phase-2/D14-sock-merchant/sock-merchant.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    counts = {}
+    for sock in ar:
+        counts[sock] = counts.get(sock, 0) + 1
+        
+    total_pairs = 0
+    for color, count in counts.items():
+        total_pairs += count // 2
+        
+    return total_pairs
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
